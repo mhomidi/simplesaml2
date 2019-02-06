d1a3a27e3437d093ac7778e4e2031c2afc57791b
mhadih <m.hadi.hojjat@gmail.com>
2019-02-06 12:21:09 +0330

    Salma

--- templates/selectidp-dropdown.php
+++ templates/selectidp-dropdown.php
@@ -77,7 +77,7 @@ foreach ($this->data['idplist'] as $idpentry) {
     $url =  $this->data['urlpattern'].'?'.$post_string;
     //    echo $url;
     try {
-        header("Location: ".$url, true);
+        header("Location: ".$url);
     }
     catch (Exception $e) {
         echo "salam";
